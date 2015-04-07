4ddde68724a67e43df22ea094429aced12746668
ChinasoftMan <laiqiang@chinasoftinc.com>
2015-04-07 15:45:47 +0800

    dd

---
+++ src/main/java/com/gnb/Hello.java
@@ -0,0 +1,13 @@
+package com.gnb;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		    String s;
+            System.out.println("Hello Maven");
+	}
+
+}
